cd2861801c0c6b2a2786f73d4da43253d91243b4
Hofer Lukas <csaz9699@student.uibk.ac.at>
2024-10-24 13:28:17 +0200

    edit Welcome page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,53 +1,56 @@
-import { Button } from '@chakra-ui/react';
-import { Link, Head } from '@inertiajs/react';
+import { Button } from "@chakra-ui/react";
+import { Link, Head } from "@inertiajs/react";
+import { VscAccount } from "react-icons/vsc";
+import { FiLogIn } from "react-icons/fi";
 
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
-
-
     return (
         <>
             <Head title="Welcome" />
 
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh',  
-                backgroundColor: 'gray',
-                flexDirection: 'column',
-                textAlign: 'center', 
-            }}>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100vh",
+                    background: "linear-gradient(to bottom, #F0E68C, #FF8C00)",
+                    flexDirection: "column",
+                    textAlign: "center",
+                }}
+            >
                 <img
                     src="images/EducoLogo.png"
                     alt="Logo"
                     style={{
-                        height: '50%',  
-                        width: 'auto',
-                        marginBottom: '20px', // Distance between image and links
+                        height: "50%",
+                        width: "auto",
+                        marginBottom: "50px", // Distance between image and links
                     }}
                 />
-                
+
                 <div>
                     {auth.user ? (
                         <Link
-                            href={route('dashboard')}
-                            className="block rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                            href={route("dashboard")}
+                            className="block rounded-md bg-orange-500 px-3 py-2 text-white ring-1 ring-transparent transition hover:bg-orange-400 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                         >
                             Dashboard
                         </Link>
                     ) : (
                         <>
                             <Link
-                                href={route('login')}
-                                className="block rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                href={route("login")}
+                                className="flex items-center block rounded-md bg-orange-500 px-3 py-2 text-white ring-1 ring-transparent transition hover:bg-orange-400 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                             >
+                                <FiLogIn className="mr-2" />
                                 Log in
                             </Link>
+
                             {/*
                             <Link
                                 href={route('register')}
-                                className="block rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                            >
+    className="block rounded-md bg-orange-500 px-3 py-2 text-white ring-1 ring-transparent transition hover:bg-orange-400 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"                            >
                                 Register
                             </Link>
                             */}
@@ -55,10 +58,9 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                     )}
                 </div>
 
-                <footer className="py-16 text-center text-sm text-black dark:text-white/70">
+                <footer className="py-8 text-center text-sm text-black dark:text-white/70">
                     Laravel v{laravelVersion} (PHP v{phpVersion})
                 </footer>
-
             </div>
         </>
     );
